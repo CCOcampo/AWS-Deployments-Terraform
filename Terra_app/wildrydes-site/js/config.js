928421b984e7c36a347390c4ac3724855ebe7424
Cristhian Ocampo <133278389+CCOcampo@users.noreply.github.com>
2025-02-19 10:33:35 -0500

    Update config.js

--- Terra_app/wildrydes-site/js/config.js
+++ Terra_app/wildrydes-site/js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_tzZ0FFhJx', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '1qq8q5bnkfdclhjjsh42bt6je6', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'us-east-1' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
